25422fdef2961e2a3811c28161e3f6998bef41d2
Dharmesh <raikwardharmesh67@gmail.com>
2025-07-22 00:59:36 +0530

    Clerk Setup with Usersync

---
+++ src/app/syncUser/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { auth,clerkClient } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
+const SyncUser = async () => {
+    const { userId } = await auth();
+    if (!userId) {
+        return <div>User not found</div>;
+    }
+    const client = await clerkClient();
+    const User = await client.users.getUser(userId);
+    if (!User) {
+        return <div>User not found</div>;
+    }
+    await db.user.upsert({
+        where: {
+            email: User.emailAddresses[0]?.emailAddress,
+        },
+        update: {
+            name: User.fullName,
+            imageUrl: User.imageUrl,
+            firstName: User.firstName,
+            lastName: User.lastName,
+        },
+        create: {
+            id: User.id,
+            email: User.emailAddresses[0]?.emailAddress,
+            name: User.fullName,
+            imageUrl: User.imageUrl,
+            firstName: User.firstName,
+            lastName: User.lastName,
+        },
+    });
+    return redirect('/dashboard');
+}
